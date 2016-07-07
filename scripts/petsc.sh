dd32940e22411e88f04f85b4784f41526d0a3173
gouarin <loic.gouarin@math.u-psud.fr>
2016-07-07 16:09:59 +0200

    fix

--- scripts/petsc.sh
+++ scripts/petsc.sh
@@ -12,6 +12,6 @@ mkdir -p $PETSC_DIR
 wget http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-$1.tar.gz
 tar xzf petsc-lite-$1.tar.gz --strip-components=1 -C $PETSC_DIR
 cd $PETSC_DIR
-./configure --with-cc=mpicc --with-cxx=mpicxx --with-debugging=0 --with-dynamic-libraries --with-fc=0 --with-shared-libraries
+./configure --with-cc=mpicc --with-cxx=mpicxx --with-debugging=0 --with-fc=0
 make PETSC_DIR=$PETSC_DIR PETSC_ARCH=$PETSC_ARCH all
 cd $CURRENT_DIR
