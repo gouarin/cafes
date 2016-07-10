d21736c165b543a54dfdcdd8e3ada6e7bed06d94
gouarin <loic.gouarin@math.u-psud.fr>
2016-07-10 08:26:18 +0200

    begin to add tests using catch

---
+++ cafes/particle/geometry/tests/box.cpp
@@ -0,0 +1,10 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include <particle/geometry/box.hpp>
+
+TEST_CASE( "Box limits", "[box]" ) {
+  cafes::geometry::box<double, 2> b1{ {0, 0}, {1, 1}};
+
+  REQUIRE( (b1.bottom_left[0] == 0 && b1.bottom_left[1] == 0) );
+  REQUIRE( (b1.upper_right[0] == 1 && b1.upper_right[1] == 1) );
+}
