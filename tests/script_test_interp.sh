a37dff95c8f1de1e773270f9a6163e56518934ab
Fabien Vergnet <fabien.vergnet@math.u-psud.fr>
2019-10-18 16:29:41 +0200

    Add test for rafinement and interpolation

---
+++ tests/script_test_interp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+exec=../build/tests/test_interp
+nproc=1
+
+# mpirun -np $nproc $exec \
+#     -pmm \
+#     -strain_tensor \
+#     -stokes_ksp_rtol 1e-4 \
+#     -stokes_ksp_atol 1e-12 \
+#     -sem_ksp_rtol 1e-2 \
+#     -sem_ksp_type fgmres \
+#     -sem_ksp_monitor_true_residual \
+#     -sem_ksp_max_it 10 \
+#     -stokes_ksp_monitor \
+#     -mx 129 \
+#     -my 129
+
+$exec \
+    -strain_tensor \
+    -stokes_ksp_type preonly \
+    -stokes_pc_type lu \
+    -sem_ksp_rtol 1e-2 \
+    -sem_ksp_type fgmres \
+    -sem_ksp_monitor_true_residual \
+    -sem_ksp_max_it 10 \
+    -assembling \
+    -mx 65 \
+    -my 65
