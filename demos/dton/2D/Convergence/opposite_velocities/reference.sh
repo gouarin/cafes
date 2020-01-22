87455a9881856cf439d8b9221669a6e02d0990b1
Fabien Vergnet <fabien.vergnet@math.u-psud.fr>
2020-01-22 15:29:56 +0100

    update demos opposite velocities

---
+++ demos/dton/2D/Convergence/opposite_velocities/reference.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+exec=../../../../../build/demos/convergence_opposite_velocities_reference
+nproc=1
+
+for i in '257'
+do
+mpirun -np $nproc $exec \
+    -assembling \
+    -stokes_ksp_type preonly \
+    -stokes_pc_type lu \
+    -stokes_ksp_monitor_true_residual \
+    -dton_ksp_type lgmres \
+    -dton_ksp_max_it 5000 \
+    -dton_ksp_rtol 1e-5 \
+    -dton_ksp_monitor_true_residual \
+    -mx $i \
+    -my $i \
+    # 
+    # -PMM \
+    #-stokes_ksp_rtol 1e-6 \
+
+done
