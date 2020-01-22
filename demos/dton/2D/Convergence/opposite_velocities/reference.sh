fdfc417241e7158130c1b406484b7313fa25000b
Fabien Vergnet <fabien.vergnet@math.u-psud.fr>
2020-01-22 19:17:44 +0100

    write reference solution in binary format

--- demos/dton/2D/Convergence/opposite_velocities/reference.sh
+++ demos/dton/2D/Convergence/opposite_velocities/reference.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 exec=../../../../../build/demos/convergence_opposite_velocities_reference
-nproc=1
+nproc=2
 
-for i in '257'
+for i in '52'
 do
 mpirun -np $nproc $exec \
     -assembling \
@@ -11,7 +11,7 @@ mpirun -np $nproc $exec \
     -stokes_pc_type lu \
     -stokes_ksp_monitor_true_residual \
     -dton_ksp_type lgmres \
-    -dton_ksp_max_it 5000 \
+    -dton_ksp_max_it 500 \
     -dton_ksp_rtol 1e-5 \
     -dton_ksp_monitor_true_residual \
     -mx $i \
