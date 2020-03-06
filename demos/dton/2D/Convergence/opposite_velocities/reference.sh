2372898b4b25c3d58c6f84a4cc9957547add43ed
Fabien Vergnet <fabien.vergnet@math.u-psud.fr>
2020-03-06 15:19:32 +0100

    modify script to launch on cluster

--- demos/dton/2D/Convergence/opposite_velocities/reference.sh
+++ demos/dton/2D/Convergence/opposite_velocities/reference.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
 exec=../../../../../build/demos/convergence_opposite_velocities_reference
-nproc=2
+nx=2049
 
-for i in '129'
-do
-mpirun -np $nproc $exec \
-    -assembling \
-    -stokes_ksp_type preonly \
-    -stokes_pc_type lu \
-    -stokes_ksp_monitor_true_residual \
-    -stokes_pc_factor_mat_solver_type superlu_dist \
-    -dton_ksp_type lgmres \
-    -dton_ksp_max_it 10000 \
-    -dton_ksp_rtol 1e-5 \
-    -dton_ksp_monitor_true_residual \
-    -mx $i \
-    -my $i \
-    # 
-    # -PMM \
-    #-stokes_ksp_rtol 1e-6 \
-
-done
+$exec \
+-assembling \
+-stokes_ksp_type preonly \
+-stokes_pc_type lu \
+-stokes_pc_factor_mat_solver_type mumps \
+-mat_mumps_icntl_2 1 \
+-mat_mumps_icntl_4 2 \
+-dton_ksp_type lgmres \
+-dton_ksp_max_it 10000 \
+-dton_ksp_rtol 1e-6 \
+-dton_ksp_monitor_true_residual \
+-mx $nx \
+-my $nx \
