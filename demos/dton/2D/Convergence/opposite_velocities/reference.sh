2035cd5f41be8b72918ff805907b2aad1840231e
Fabien Vergnet <fabien.vergnet@math.u-psud.fr>
2020-03-18 15:15:16 +0100

    changes for cluster

--- demos/dton/2D/Convergence/opposite_velocities/reference.sh
+++ demos/dton/2D/Convergence/opposite_velocities/reference.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 exec=../../../../../build/demos/convergence_opposite_velocities_reference
-nx=257
+nx=2048
 
 $exec \
 -assembling \
 -stokes_ksp_type preonly \
 -stokes_pc_type lu \
--stokes_pc_factor_mat_solver_type petsc \
+-stokes_pc_factor_mat_solver_type mumps \
 -mat_mumps_icntl_2 1 \
 -mat_mumps_icntl_4 2 \
 -dton_ksp_type lgmres \
